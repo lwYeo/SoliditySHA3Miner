3edde9f0b872ff00e14a80eb06275ea2b9ff1eff
Lip Wee Yeo Amano <Lwyeo001@gmail.com>
2018-09-16 17:12:46 +0800

    Added launch scripts for ethOS and guide for ethOS & HiveOS

---
+++ SoliditySHA3Miner/0xbtcPool-ethOS-screen.sh
@@ -0,0 +1,8 @@
+command -v dotnet >/dev/null 2>&1 ||
+{
+ echo >&2 ".NET Core is not found or not installed,"
+ echo >&2 "run 'sh install-deps-ethOS.sh' to install dependencies.";
+ read -p "Press any key to continue...";
+ exit 1;
+}
+screen -S SoliditySHA3Miner dotnet $(pwd)/SoliditySHA3Miner.dll abiFile=0xbtc.abi contract=0xB6eD7644C69416d67B522e20bC294A9a9B405B31 overrideMaxTarget=27606985387162255149739023449108101809804435888681546220650096895197184 pool=http://mike.rs:8080 address=0x9172ff7884CEFED19327aDaCe9C470eF1796105c
--- SoliditySHA3Miner/0xbtcPool-ethOS.sh
+++ SoliditySHA3Miner/0xbtcPool-ethOS.sh
@@ -1,7 +1,7 @@
 command -v dotnet >/dev/null 2>&1 ||
 {
  echo >&2 ".NET Core is not found or not installed,"
- echo >&2 "run 'sh install-deps.sh' to install dependencies.";
+ echo >&2 "run 'sh install-deps-ethOS.sh' to install dependencies.";
  read -p "Press any key to continue...";
  exit 1;
 }
