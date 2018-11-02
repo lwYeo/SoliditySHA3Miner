473c9c5495d3f361531cb44bbf85b4e98443cf9e
lwYeo <Lwyeo001@gmail.com>
2018-11-02 10:42:29 +0800

    Reduce Intel iGPU intensity to 16 to improve long term stability

--- OpenCLSoliditySHA3Solver/device/device.cpp
+++ OpenCLSoliditySHA3Solver/device/device.cpp
@@ -510,7 +510,7 @@ namespace OpenCLSolver
 
 	void Device::setIntensity(float const intensity, bool isKingMaking)
 	{
-		if (isINTEL()) userDefinedIntensity = (intensity > 1.0f) ? intensity : 18.0f; // iGPU
+		if (isINTEL()) userDefinedIntensity = (intensity > 1.0f) ? intensity : 16.0f; // iGPU
 		else userDefinedIntensity = (intensity > 1.0f) ? intensity : (isKingMaking ? DEFAULT_INTENSITY_KING : DEFAULT_INTENSITY);
 
 		auto userTotalWorkSize = (uint32_t)std::pow(2, userDefinedIntensity);
